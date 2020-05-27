3726b260944445c5fd0e23bf578f69bd6bed1e47
tbriggs1 <tombriggs93@protonmail.com>
2020-05-27 08:32:37 +0100

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tomb1\documents\repo\web_design\django_angular\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
